cd421a48eed067acde2c443172a9072b46653a0c
patowhiz <conlooptechnologies@gmail.com>
2023-10-31 14:34:46 +0300

    final

--- instat/ucrObjectTypeSelector.vb
+++ instat/ucrObjectTypeSelector.vb
@@ -11,24 +11,26 @@ Public Class ucrObjectTypeSelector
         ' Add any initialization after the InitializeComponent() call.
 
 
-        'set up the combobox for object types
-        Dim dct As New Dictionary(Of String, String) From {
+        'set up the combobox fwith default object types
+        SetObjectTypes(New Dictionary(Of String, String) From {
             {"object", "Objects"},
             {RObjectTypeLabel.Summary, "Summaries"},
             {RObjectTypeLabel.Table, "Tables"},
             {RObjectTypeLabel.Graph, "Graphs"},
             {RObjectTypeLabel.Model, "Models"},
-            {RObjectTypeLabel.StructureLabel, "Structured"}}
-
-        cboObjectType.DataSource = New BindingSource(dct, Nothing)
-        cboObjectType.ValueMember = "Key"
-        cboObjectType.DisplayMember = "Value"
+            {RObjectTypeLabel.StructureLabel, "Structured"}})
         cboObjectType.SelectedIndex = 0
     End Sub
 
     Private Sub cboObjectType_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboObjectType.SelectedIndexChanged
         OnControlContentsChanged()
         OnControlValueChanged()
+    End Sub
+
+    Public Sub SetObjectTypes(dctObjectTypes As Dictionary(Of String, String))
+        cboObjectType.DataSource = New BindingSource(dctObjectTypes, Nothing)
+        cboObjectType.ValueMember = "Key"
+        cboObjectType.DisplayMember = "Value"
     End Sub
 
     Public Function GetSelectedValue() As String
